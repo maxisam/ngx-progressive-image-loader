f9b882c833ff5e3004f45b700a13af6747271e2f
maxisam <maxisam@gmail.com>
2018-08-29 14:42:18 -0500

    import from root

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgxProgressiveImageLoaderModule } from 'ngx-progressive-image-loader';
-import { IImageLoaderOptions } from 'ngx-progressive-image-loader/lib/config';
+import { IImageLoaderOptions, NgxProgressiveImageLoaderModule } from 'local-ngx-progressive-image-loader';
 
 import { AppComponent } from './app.component';
 
