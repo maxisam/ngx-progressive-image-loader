ccd3243a66376e613cdfcd8af48c499e15ad9c03
maxisam <maxisam@gmail.com>
2018-08-29 11:31:35 -0500

    move default placeHolderImage out of package

--- projects/ngx-progressive-image-loader/src/lib/config.ts
+++ projects/ngx-progressive-image-loader/src/lib/config.ts
@@ -1,7 +1,7 @@
 import { InjectionToken } from '@angular/core';
 
 export interface IImageLoaderOptions extends IntersectionObserverInit {
-  placeHolderImage: string;
+  placeHolderImage?: string;
   imageRatio: number;
   blurFilter: number;
 }
@@ -13,7 +13,5 @@ export const DEFAULT_IMAGE_LOADER_OPTIONS = <IImageLoaderOptions>{
   threshold: 0.5,
   imageRatio: 16 / 9,
   blurFilter: 0,
-  placeHolderImage:
-    // tslint:disable-next-line:max-line-length
-    'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICA8cGF0aCBmaWxsPSIjZGQwMDMxIiBkPSJNMTI1IDMwTDMxLjkgNjMuMmwxNC4yIDEyMy4xTDEyNSAyMzBsNzguOS00My43IDE0LjItMTIzLjF6Ii8+CiAgPHBhdGggZmlsbD0iI2MzMDAyZiIgZD0iTTEyNSAzMHYyMi4yLS4xVjIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMUwxMjUgMzB6Ii8+CiAgPHBhdGggZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiBmaWxsPSIjZmZmIi8+Cjwvc3ZnPgo='
+  placeHolderImage: ''
 };
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxProgressiveImageLoaderModule } from 'ngx-progressive-image-loader';
+import { IImageLoaderOptions } from 'ngx-progressive-image-loader/lib/config';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NgxProgressiveImageLoaderModule.forRoot()],
+  imports: [
+    BrowserModule,
+    NgxProgressiveImageLoaderModule.forRoot(<IImageLoaderOptions>{
+      placeHolderImage:
+        // tslint:disable-next-line:max-line-length
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICA8cGF0aCBmaWxsPSIjZGQwMDMxIiBkPSJNMTI1IDMwTDMxLjkgNjMuMmwxNC4yIDEyMy4xTDEyNSAyMzBsNzguOS00My43IDE0LjItMTIzLjF6Ii8+CiAgPHBhdGggZmlsbD0iI2MzMDAyZiIgZD0iTTEyNSAzMHYyMi4yLS4xVjIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMUwxMjUgMzB6Ii8+CiAgPHBhdGggZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiBmaWxsPSIjZmZmIi8+Cjwvc3ZnPgo='
+    })
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
