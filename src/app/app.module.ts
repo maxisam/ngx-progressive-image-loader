71350f81de1516fde7280c4912c7b9367f83422c
maxisam <maxisam@gmail.com>
2018-08-30 11:04:56 -0500

    chore: update demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,14 @@ import { IImageLoaderOptions, NgxProgressiveImageLoaderModule } from 'ngx-progre
 
 import { AppComponent } from './app.component';
 
+// import { IImageLoaderOptions, NgxProgressiveImageLoaderModule } from 'local-ngx-progressive-image-loader';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     NgxProgressiveImageLoaderModule.forRoot(<IImageLoaderOptions>{
+      filter: 'blur(3px) drop-shadow(0 0 0.75rem crimson)',
       placeHolderImage:
         // tslint:disable-next-line:max-line-length
         'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICA8cGF0aCBmaWxsPSIjZGQwMDMxIiBkPSJNMTI1IDMwTDMxLjkgNjMuMmwxNC4yIDEyMy4xTDEyNSAyMzBsNzguOS00My43IDE0LjItMTIzLjF6Ii8+CiAgPHBhdGggZmlsbD0iI2MzMDAyZiIgZD0iTTEyNSAzMHYyMi4yLS4xVjIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMUwxMjUgMzB6Ii8+CiAgPHBhdGggZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiBmaWxsPSIjZmZmIi8+Cjwvc3ZnPgo='
