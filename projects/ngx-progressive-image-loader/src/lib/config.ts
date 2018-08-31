6801b3e91488c7207157d51c637a6f9a09515d90
maxisam <maxisam@gmail.com>
2018-08-31 09:33:18 -0500

    change default setting

--- projects/ngx-progressive-image-loader/src/lib/config.ts
+++ projects/ngx-progressive-image-loader/src/lib/config.ts
@@ -11,8 +11,8 @@ export const IMAGE_LOADER_CONFIG_TOKEN = new InjectionToken<IImageLoaderOptions>
 
 export const DEFAULT_IMAGE_LOADER_OPTIONS = <IImageLoaderOptions>{
   // root?: Element | null;
-  rootMargin: '0px',
-  threshold: 0.5,
+  rootMargin: '10px',
+  threshold: 0.1,
   imageRatio: 16 / 9,
   blurFilter: 0,
   placeHolderImage: ''
